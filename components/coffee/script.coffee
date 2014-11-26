2c3c388f9e0adbcb07a4f6f1c55033ff1b55683b
Brennan King <brennanhking@gmail.com>
2014-11-25 19:12:37 -0600

    Adds resume cards and images

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,6 +1,13 @@
 $ ->
   $("#experience").click ->
-    $(".elevator-pitch").fadeOut(200, ->
-      $(".nav-initial").addClass("nav-secondary")
-      $(".projects").addClass("projects-hidden")
+    $(".elevator-pitch").fadeOut(500, ->
+      $(".projects").fadeOut("slow")
+      $(".nav-initial").delay(500).addClass("nav-secondary")
+      $("#resume").delay(1000).fadeIn("fast")
+    )
+
+  $("#nav-back").click ->
+    $("#resume").fadeOut(500, ->
+      $(".nav-initial").delay(500).removeClass("nav-secondary")
+      $(".elevator-pitch").delay(1000).fadeIn("fast")
     )
--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,8 +1,15 @@
 $(function() {
-  return $("#experience").click(function() {
-    return $(".elevator-pitch").fadeOut(200, function() {
-      $(".nav-initial").addClass("nav-secondary");
-      return $(".projects").addClass("projects-hidden");
+  $("#experience").click(function() {
+    return $(".elevator-pitch").fadeOut(500, function() {
+      $(".projects").fadeOut("slow");
+      $(".nav-initial").delay(500).addClass("nav-secondary");
+      return $("#resume").delay(1000).fadeIn("fast");
+    });
+  });
+  return $("#nav-back").click(function() {
+    return $("#resume").fadeOut(500, function() {
+      $(".nav-initial").delay(500).removeClass("nav-secondary");
+      return $(".elevator-pitch").delay(1000).fadeIn("fast");
     });
   });
 });
